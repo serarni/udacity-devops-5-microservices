e9629f4846d7373c7e0a95650f4f25d9b3e0841b
Sergio Arnillas <serarni@gmail.com>
2020-04-26 21:57:05 +0200

    Updating new version and increase time for rolling out deployment

--- app.py
+++ app.py
@@ -64,7 +64,7 @@ def predict():
     prediction = list(clf.predict(scaled_payload))
     # Log the output prediction value
     LOG.info(f"return prediction: %s",prediction)
-    return jsonify({'prediction': prediction, 'version':'5'})
+    return jsonify({'prediction': prediction, 'version':'6'})
 
 if __name__ == "__main__":
     # load pretrained model as clf
--- aws/scripts/terminate_instances.py
+++ aws/scripts/terminate_instances.py
@@ -12,6 +12,7 @@ for r in reservations:
 
     for o in data:
         if o["State"]["Code"]!=48: # 48 means terminated
-           # print( o["InstanceId"])
+           print( o["InstanceId"] + ' instance will be termintated now')
            os.system('aws ec2 terminate-instances --instance-ids ' + o["InstanceId"])
-           time.sleep(60*3) # wait 3 minutes to stop next instance to let time enought to start again the new instance
+           print( ' Waiting 10 minutes to terminate next instance')
+           time.sleep(60*10) # wait 3 minutes to stop next instance to let time enought to start again the new instance

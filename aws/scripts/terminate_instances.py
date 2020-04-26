c6706b9f8a40448f15dfd382991f5a4055da2703
Sergio Arnillas <serarni@gmail.com>
2020-04-26 19:27:45 +0200

    Wainting to instance to restart before to kill the next one

--- aws/scripts/terminate_instances.py
+++ aws/scripts/terminate_instances.py
@@ -1,6 +1,7 @@
 import json
 import sys
 import os
+import time
 
 
 jdata = sys.stdin.read()
@@ -13,3 +14,4 @@ for r in reservations:
         if o["State"]["Code"]!=48: # 48 means terminated
            # print( o["InstanceId"])
            os.system('aws ec2 terminate-instances --instance-ids ' + o["InstanceId"])
+           time.sleep(60*3) # wait 3 minutes to stop next instance to let time enought to start again the new instance
